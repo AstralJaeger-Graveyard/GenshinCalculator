c1ab9540f605794eed62ae4cffb858d0ae75ea6e
Felix Hillebrand <97hilfel@gmail.com>
2020-11-17 22:07:10 +0100

    Add motto and card field, clanup constructor of Character

--- src/app/model/Character.ts
+++ src/app/model/Character.ts
@@ -4,20 +4,20 @@ import { WeaponType } from './WeaponType';
 export class Character{
   public name: string;
   public description: string;
+  public motto: string;
   public element: Element;
   public weaponType: WeaponType;
   public rarity: number;
   public tier: string;
   public portrait: string;
+  public card: string;
   public icon: string;
   public isNew: boolean = false;
-  // TODO: Upgrade materials, talent materials
+  // TODO: Ascension materials, talent materials
 
-  constructor(name: string, description: string, element: Element, rarity: number, tier: string) {
+  constructor(name: string, description: string, element: Element) {
     this.name = name;
     this.description = description;
     this.element = element;
-    this.rarity = rarity;
-    this.tier = tier;
   }
 }
