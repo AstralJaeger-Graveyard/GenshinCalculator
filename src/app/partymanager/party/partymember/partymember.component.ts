7e1a84572c1a94b4f2ed780615db50020c1be648
Felix Hillebrand <97hilfel@gmail.com>
2020-11-17 17:04:57 +0100

    Initial styling of partymember, add edit and remove function

--- src/app/partymanager/party/partymember/partymember.component.ts
+++ src/app/partymanager/party/partymember/partymember.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {PartyMember} from '../../../model/PartyMember';
+import {Element} from '../../../model/Element';
+import { faInfoCircle, faEdit, faTrashAlt } from '@fortawesome/pro-light-svg-icons';
 
 @Component({
   selector: 'app-partymember',
@@ -8,11 +10,18 @@ import {PartyMember} from '../../../model/PartyMember';
 })
 export class PartymemberComponent implements OnInit {
 
+  faInfo = faInfoCircle;
+  faEdit = faEdit;
+  faTrash = faTrashAlt;
+
   @Input()
   member: PartyMember;
 
   @Output()
   memberSelected = new EventEmitter<void>();
+
+  @Output()
+  memberRemoved = new EventEmitter<void>();
 
   constructor() { }
 
@@ -21,5 +30,28 @@ export class PartymemberComponent implements OnInit {
 
   onSelected(): void{
     this.memberSelected.emit();
+  }
+
+  onRemove(): void{
+    this.memberRemoved.emit();
+  }
+
+  resolveElement(element: Element): string{
+    switch (element){
+      case Element.Anemo:
+        return "Anemo";
+      case Element.Cryo:
+        return "Cryo";
+      case Element.Electro:
+        return "Electro";
+      case Element.Dendro:
+        return "Dendro";
+      case Element.Geo:
+        return "Geo";
+      case Element.Hydro:
+        return "Hydro";
+      case Element.Pyro:
+        return "Pyro";
+    }
   }
 }
