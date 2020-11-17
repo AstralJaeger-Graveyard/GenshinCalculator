17596812bd412df2c74430398728a1d66de58daa
Felix Hillebrand <97hilfel@gmail.com>
2020-11-17 17:03:46 +0100

    Add weapon type to character data model

--- src/app/model/Character.ts
+++ src/app/model/Character.ts
@@ -1,15 +1,17 @@
-import {Element} from './Element';
+import { Element } from './Element';
+import { WeaponType } from './WeaponType';
 
 export class Character{
   public name: string;
   public description: string;
   public element: Element;
+  public weaponType: WeaponType;
   public rarity: number;
   public tier: string;
   public portrait: string;
   public icon: string;
   public isNew: boolean = false;
-  // TODO: Upgrade materials, talent materials, weapon
+  // TODO: Upgrade materials, talent materials
 
   constructor(name: string, description: string, element: Element, rarity: number, tier: string) {
     this.name = name;
