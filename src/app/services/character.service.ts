5202f4130ed501e5758702583a5819ed0416e893
Felix Hillebrand <97hilfel@gmail.com>
2020-11-20 10:17:32 +0100

    Add getters for characters service

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../model/Character';
-import { HttpClient } from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import * as  cData from 'src/app/_dataassets/characters.json';
-
+import * as cData from 'src/app/_dataassets/characters.json';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CharacterService {
-  private dataUrl = 'assets/characters.json';
-
+export class CharacterService{
   // @ts-ignore
-  public characters: Character[] = cData.default;
+  private _characters: Character[] = cData.default;
+  private _characterMap: Map<string, Character> = new Map<string, Character>();
 
-  public characterMap: Map<string, Character> = new Map<string, Character>();
+  constructor() { }
 
-  constructor() {
-    this.characters.forEach(character => {
-      this.characterMap.set(character.name.toLowerCase(), character)
-    });
+  public get characters(){
+    return this._characters;
+  }
+
+  public get characterMap(){
+    if(this._characterMap.size === 0){
+      for(let character of this._characters){
+        this._characterMap.set(character.name.toLowerCase(), character);
+      }
+    }
+    return this._characterMap
   }
 }
