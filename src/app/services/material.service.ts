804a6382ee7366dbbc31c8386d95082c646caf5a
Felix Hillebrand <97hilfel@gmail.com>
2020-11-23 18:41:40 +0100

    Implement material service

---
+++ src/app/services/material.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../model/Character';
+import {Material} from '../model/Material';
+import * as data from 'src/app/_dataassets/data.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MaterialService{
+
+  // @ts-ignore
+  public materials: Material[] = data.materials;
+  private materialsMap: Map<string, Material> = new Map<string, Material>();
+
+  constructor() { }
+
+  public get getMaterialsMap(){
+    if(this.materialsMap.size === 0){
+      for(let material of this.materials){
+        this.materialsMap.set(material.id, material);
+      }
+    }
+    return this.materialsMap
+  }
+
+  public resolveMaterials(character: Character): void{
+    for (let mset of character.ascension){
+      for (let material of mset.materials){
+        console.log("Infilling: " + character.name + " Material: " + material.material_id);
+        material.material = this.getMaterialsMap.get(material.material_id);
+      }
+    }
+  }
+}
