76509d12feb6aa5c15a6e909fd47ac3e92522336
Felix Hillebrand <97hilfel@gmail.com>
2020-11-24 19:03:16 +0100

    Set ascension stage for default party to 0

--- src/app/services/party.service.ts
+++ src/app/services/party.service.ts
@@ -16,10 +16,10 @@ export class PartyService{
     // TODO: Load party form cookie/firebase
     if (this.party.length === 0){
       loggingService.log('Initializing with default data', null)
-      this.party.push(new PartyMember(this.characterService.getCharacterMap.get('traveler (anemo)'), 1, 1));
-      this.party.push(new PartyMember(this.characterService.getCharacterMap.get('amber'), 1, 1));
-      this.party.push(new PartyMember(this.characterService.getCharacterMap.get('kaeya'), 1, 1));
-      this.party.push(new PartyMember(this.characterService.getCharacterMap.get('lisa'), 1, 1));
+      this.party.push(new PartyMember(this.characterService.getCharacterMap.get('traveler (anemo)'), 1, 0));
+      this.party.push(new PartyMember(this.characterService.getCharacterMap.get('amber'), 1, 0));
+      this.party.push(new PartyMember(this.characterService.getCharacterMap.get('kaeya'), 1, 0));
+      this.party.push(new PartyMember(this.characterService.getCharacterMap.get('lisa'), 1, 0));
     }
   }
 }
