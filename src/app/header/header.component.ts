52aef37428c209c8b8309e9681019acf25b8417c
Felix Hillebrand <97hilfel@gmail.com>
2020-12-14 19:14:58 +0100

    Add analytics to discord invite and kofi link

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PartyService} from '../services/party.service';
+import {GoogleAnalyticsService} from 'ngx-google-analytics';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public gAnalytics: GoogleAnalyticsService) { }
 
   ngOnInit(): void {
   }
 
+  onKoFiClick() {
+    this.gAnalytics.event('ko-fi_click', 'Header', 'ko-fi');
+  }
+
+  onDiscordClick() {
+    this.gAnalytics.event('discord_click', 'Header', 'discord_invite');
+  }
 }
