fe8122ba80ee0033129ea60f47e5a81ecd98cab2
Felix Hillebrand <97hilfel@gmail.com>
2020-12-28 14:51:59 +0100

    Add data model for artifacts

---
+++ src/app/model/Artifact.ts
@@ -0,0 +1,8 @@
+import {MaterialSource} from './MaterialSource';
+
+export class Artifact {
+  public id: string;
+  public setId: string;
+  public name: string;
+  public sources: MaterialSource[];
+}
