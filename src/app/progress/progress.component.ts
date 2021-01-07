77631333b8ba714b1e98ab33ac9ec266b5272ca7
Felix Hillebrand <97hilfel@gmail.com>
2021-01-07 18:30:17 +0100

    Update progression

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -7,42 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
-  public currentState = 1;
+  public currentState = 2;
 
   public upcomingFeatures = [
     {
-      title: "Artifacts",
-      description: "Why did it roll into DEF again?"
+      title: 'Artifacts',
+      description: 'Why did it roll into DEF again?'
     },
     {
-      title: "Schedule",
-      description: "When did I have to grind that anoying domain again?"
+      title: 'Schedule',
+      description: 'When did I have to grind that anoying domain again?'
     },
     {
-      title: "Talents",
-      description: "Never know who's material is due on what day? Fear no more!"
+      title: 'Talents',
+      description: 'Never know who\'s material is due on what day? Fear no more!'
     },
     {
-      title: "Dark mode",
-      description: "Who dislikes light themes aswell?"
+      title: 'Dark mode',
+      description: 'Who dislikes light themes aswell?'
     },
     {
-      title: "Signin",
-      description: "Now you can signin!"
+      title: 'Signin',
+      description: 'Now you can signin!'
     },
     {
-      title: "Premium",
-      description: "A way to store things in the cloud! And support me through the app!"
+      title: 'Premium',
+      description: 'A way to store things in the cloud! And support me through the app!'
     },
     {
-      title: "Server Side Rendering",
-      description: "Make everything a little faster!"
+      title: 'Server Side Rendering',
+      description: 'Make everything a little faster!'
     },
     {
-      title: "Enable PWA",
-      description: "Optimize for mobile devices!"
+      title: 'Enable PWA',
+      description: 'Optimize for mobile devices!'
     }
-  ]
+  ];
 
   constructor() { }
 
